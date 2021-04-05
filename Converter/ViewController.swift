6b2c8ec4bd30c0e281869f1731e19511211ff747
7se7olod <pushin_95@mail.ru>
2021-04-05 17:38:48 +0400

    2

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var temperatureCelsius: UILabel!
+    @IBOutlet weak var temperatureFahrenheit: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 50
+            slider.minimumValue = -50
+            slider.value = 0
+        }
     }
-
-
+    
+    @IBAction func slideChanged(_ sender: UISlider) {
+        let temperatureCe = Int(sender.value)
+        temperatureCelsius.text = "\(temperatureCe)ºC"
+        let temperatureFa = Int16(sender.value * 1.8 + 32)
+        temperatureFahrenheit.text = "\(temperatureFa)ºF"
+    }
+    
+    
 }
 
